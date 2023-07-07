94ab0dbdca4932c360a771ecb51efc877625d704
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-07 16:31:03 +0300

    today last commit

---
+++ miniProject.js
@@ -0,0 +1,18 @@
+
+
+const population = document.querySelector('.population')
+const languages = document.querySelector('.languages')
+const para = document.querySelector('p')
+
+population.addEventListener('click', () =>{
+    para.textContent = '10 Most populated countries in the world'
+    para.textContent = countries_data
+})
+
+
+languages.addEventListener('click', () =>{
+    para.textContent = '10 Most spoken languages in the world'
+})
+
+
+import countries_data from "./countries";
